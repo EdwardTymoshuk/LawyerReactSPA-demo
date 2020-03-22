cfabc655f8cc737c080398ac7f8c283d74db1faa
Edward Tymoshuk <eduard.tymoshuk@gmail.com>
2020-03-22 18:46:09 +0100

    Comm

---
+++ precache-manifest.4736ee5ba415c15fd600ce2d7499f1a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c4ef0458f6610b1d736a88f2b2f990",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab35ba34624fa97c49de",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1c00ecb13270f2e47a1b",
+    "url": "/static/css/3.3db4cf8d.chunk.css"
+  },
+  {
+    "revision": "9e467516dc4b75400e65",
+    "url": "/static/css/main.1d20498e.chunk.css"
+  },
+  {
+    "revision": "ab35ba34624fa97c49de",
+    "url": "/static/js/2.ca219d0d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ca219d0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c00ecb13270f2e47a1b",
+    "url": "/static/js/3.a6b7c686.chunk.js"
+  },
+  {
+    "revision": "9e467516dc4b75400e65",
+    "url": "/static/js/main.d1405c76.chunk.js"
+  },
+  {
+    "revision": "b00892857e48fea7eb93",
+    "url": "/static/js/runtime-main.c33c0e23.js"
+  },
+  {
+    "revision": "b71c9e01631ed0c286f3d67693079e98",
+    "url": "/static/media/preloader.b71c9e01.svg"
+  }
+]);
